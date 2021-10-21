9f732173de487174989df7cb5db122d12c5e8454
= <=>
2021-10-21 14:16:52 +0200

    ex

--- components/Categories/index.js
+++ components/Categories/index.js
@@ -1,73 +1,20 @@
 import CategoriasBox from "/components/CategoriasBox";
+import { CategoyData, numItems } from "../../data/home"
 
 const index = () => {
   return (
     <>
       <div className="container">
-        <CategoriasBox
-          link="/posts/sillas-gamers"
-          src="/silla-gaming.jpg"
-          alt="silla gaming"
-          title="Sillas para gamers"
-          width={151.5}
-          height={250}
+        {numItems.map((numItems, index) => (
+          <CategoriasBox
+          link={CategoyData[index].link}
+          src={CategoyData[index].src}
+          alt={CategoyData[index].alt}
+          title={CategoyData[index].title}
+          width={CategoyData[index].width}
+          height={CategoyData[index].height}
         />
-        <CategoriasBox
-          link="/posts/sillas-oficina"
-          src="/silla-oficina.jpg"
-          alt="silla de oficina"
-          title="Sillas de oficina"
-          width={151.5}
-          height={250}
-        />
-        <CategoriasBox
-          link="/"
-          src="/puff.jpg"
-          alt="puff rojo"
-          title="Puffs"
-          width={238.167}
-          height={250}
-        />
-        <CategoriasBox
-          link="/"
-          src="/silla-comedor.jpg"
-          alt="silla de comedor de diseño"
-          title="Sillas de comedor"
-          width={151.5}
-          height={250}
-        />
-        <CategoriasBox
-          link="/"
-          src="/silla-exterior.jpg"
-          alt="silla de exterior"
-          title="Sillas de exterior"
-          width={151.5}
-          height={200}
-        />
-        <CategoriasBox
-          link="/"
-          src="/taburete.jpg"
-          alt="taburete de cocina"
-          title="Taburtes para barra/cocina"
-          width={210}
-          height={250}
-        />
-        <CategoriasBox
-          link="/"
-          src="/sillon-masaje.jpg"
-          alt="sillon de masaje"
-          title="Sillones de masaje"
-          width={250}
-          height={250}
-        />
-        <CategoriasBox
-          link="/"
-          src="/silla-mecedora.jpg"
-          alt="silla mecedora"
-          title="Sillas mecedoras"
-          width={235}
-          height={250}
-        />
+          ))}
       </div>
 
       <style jsx>{`
@@ -127,7 +74,6 @@ const index = () => {
             margin-left: 150px;
             margin-right: 150px;
           }
-
         }
 
         @media only screen and (min-width: 1600px) {
@@ -136,7 +82,6 @@ const index = () => {
             margin-left: 250px;
             margin-right: 250px;
           }
-
         }
       `}</style>
     </>

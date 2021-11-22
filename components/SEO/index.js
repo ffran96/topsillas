55cad7c4d09e871c0161c93873887498eb44bd4c
= <=>
2021-11-22 16:16:44 +0100

    seo

--- components/SEO/index.js
+++ components/SEO/index.js
@@ -4,20 +4,35 @@ export default function index(props) {
   return (
     <Head>
       <meta name="viewport" content="width=device-width, initial-scale=1" />
-      <meta name='robots' content='index, follow, max-image-preview:large, max-snippet:-1, max-video-preview:-1' />
-      
+      <meta
+        name="robots"
+        content="index, follow, max-image-preview:large, max-snippet:-1, max-video-preview:-1"
+      />
+
       <title>{props.title} | TopSillas</title>
       <meta name="description" content={props.description} />
-      <link rel="canonical" href={"https://www.topsillas.info/"+props.url} />
+      <link rel="canonical" href={"https://www.topsillas.info/" + props.url} />
       <meta property="og:locale" content="es_ES" />
       <meta property="og:type" content="website" />
-      <meta property="og:title" content={props.title+" | TopSillas"} />
+      <meta property="og:title" content={props.title + " | TopSillas"} />
       <meta property="og:description" content={props.description} />
-      <meta property="og:url" content={"https://www.topsillas.info/"+props.url} />
+      <meta
+        property="og:url"
+        content={"https://www.topsillas.info/" + props.url}
+      />
       <meta property="og:site_name" content="Topsillas.info" />
+      <meta
+        property="og:image"
+        content={props.img}
+      />
+      <meta property="og:image:width" content={props.width} />
+      <meta property="og:image:height" content={props.height} />
 
-      <meta name="google-site-verification" content="kemFhMXigsPX2nbXARC9C_mgBc9n0W9PQjXvTzM2kV4" />
-      <meta name="theme-color" content="#000000"/>
+      <meta
+        name="google-site-verification"
+        content="kemFhMXigsPX2nbXARC9C_mgBc9n0W9PQjXvTzM2kV4"
+      />
+      <meta name="theme-color" content="#000000" />
       <link rel="icon" href="/favicon.ico" />
       <link
         rel="stylesheet"
--- pages/index.js
+++ pages/index.js
@@ -13,6 +13,9 @@ export default function Home() {
         url=""
         title="Inicio"
         description="¡Encuentra tu silla ideal al mejor precio y calidad!"
+        img="https://www.topsillas.info/_next/image?url=%2Fsilla-comedor.jpg&w=384&q=75"
+        width="644px"
+        height="1109px"
       />
       <Navbar />
       <Categories />

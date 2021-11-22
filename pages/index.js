e9255a06e026784919df4ff752551b8578ac5168
= <=>
2021-11-22 16:19:53 +0100

    seo

--- pages/index.js
+++ pages/index.js
@@ -14,8 +14,8 @@ export default function Home() {
         title="Inicio"
         description="¡Encuentra tu silla ideal al mejor precio y calidad!"
         img="https://www.topsillas.info/_next/image?url=%2Fsilla-comedor.jpg&w=384&q=75"
-        width="644px"
-        height="1109px"
+        width="644"
+        height="1109"
       />
       <Navbar />
       <Categories />

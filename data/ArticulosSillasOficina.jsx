ecfaf4f0f69e3e5077b0cb8826f8452ccb146db4
= <=>
2021-11-23 22:35:31 +0100

    update

--- data/ArticulosSillasOficina.jsx
+++ data/ArticulosSillasOficina.jsx
@@ -1,18 +1,18 @@
 export const ArtsSillasOficina = [
-    {
-        title: "Top 5 Mejores sillas de oficina relación calidad precio",
-        text: (
-          <>
-            <p>
-              Lorem ipsum dolor sit, amet consectetur adipisicing elit. Laudantium
-              ex illum quod qui rerum. Repellendus amet odit asperiores placeat quia
-              dolorum, fugiat voluptas, quaerat molestiae magnam eos veniam deleniti
-              veritatis.
-            </p>
-          </>
-        ),
-        link: "/sillas-de-oficina/mejores-sillas-de-oficina-calidad-precio",
-        src: "oficina",
-        alt: "oficina con sillas de oficina",
-      },      
-]
+  {
+    title: "Top 5 Mejores sillas de oficina relación calidad precio",
+    text: (
+      <>
+        <p>
+          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Laudantium
+          ex illum quod qui rerum. Repellendus amet odit asperiores placeat quia
+          dolorum, fugiat voluptas, quaerat molestiae magnam eos veniam deleniti
+          veritatis.
+        </p>
+      </>
+    ),
+    link: "/sillas-de-oficina/mejores-sillas-de-oficina-calidad-precio",
+    src: "oficina",
+    alt: "oficina con sillas de oficina",
+  },
+];
--- data/ArtsComedor.jsx
+++ data/ArtsComedor.jsx
@@ -1,18 +1,18 @@
 export const ArtsComedor = [
-    {
-        title: "El mejor pack de 6 sillas modernas de comedor de 2021",
-        text: (
-          <>
-            <p>
-              Lorem ipsum dolor sit, amet consectetur adipisicing elit. Laudantium
-              ex illum quod qui rerum. Repellendus amet odit asperiores placeat quia
-              dolorum, fugiat voluptas, quaerat molestiae magnam eos veniam deleniti
-              veritatis.
-            </p>
-          </>
-        ),
-        link: "/sillas-de-comedor/el-mejor-pack-de-6-sillas-modernas-de-comedor-de-2021",
-        src: "comedor",
-        alt: "Mesas y sillas de comedor",
-      },      
-]
+  {
+    title: "El mejor pack de 6 sillas modernas de comedor de 2021",
+    text: (
+      <>
+        <p>
+          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Laudantium
+          ex illum quod qui rerum. Repellendus amet odit asperiores placeat quia
+          dolorum, fugiat voluptas, quaerat molestiae magnam eos veniam deleniti
+          veritatis.
+        </p>
+      </>
+    ),
+    link: "/sillas-de-comedor/el-mejor-pack-de-6-sillas-modernas-de-comedor-de-2021",
+    src: "comedor",
+    alt: "Mesas y sillas de comedor",
+  },
+];

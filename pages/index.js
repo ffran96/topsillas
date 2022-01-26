967713f4330b20b41924a598a802fa32789c0596
= <=>
2022-01-26 10:40:34 +0100

    actu

--- pages/index.js
+++ pages/index.js
@@ -11,7 +11,7 @@ export default function Home() {
     <>
       <SEO
         url=""
-        title="Inicio"
+        title="Las mejores reseñas y comparativas sobre sillas"
         description="Ofertas 🛒, chollos 💸, primeras marcas 🔝, reseñas ✍... TODO lo que quieras saber sobre la silla que buscas lo tienes en Topsillas.info ▶ ¡No compres tu silla, sofá o sillón hasta conocer nuestra opinión 😁 !"
         img="https://www.topsillas.info/_next/image?url=%2Fsilla-comedor.jpg&w=384&q=75"
         width="644"

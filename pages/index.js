ab7be02d91d900aa195dc6fb9929fd2dfe023111
Fran <=>
2022-12-08 16:14:09 +0100

    Titulo del inicio y descripcion

--- pages/index.js
+++ pages/index.js
@@ -11,8 +11,8 @@ export default function Home({ categories }) {
         title="Las mejores reseñas y comparativas sobre sillas"
         description="Las mejores ofertas, precios y chollos los encontrarás en Top Sillas TODO lo que quieras saber sobre la silla que buscas lo tienes en Topsillas.info"
         img="https://www.topsillas.info/_next/image?url=https%3A%2F%2Fwp.topsillas.info%2Fwp-content%2Fuploads%2F2022%2F11%2Fsillas-gaming.jpg&w=256&q=75"
-        width="644"
-        height="1109"
+        width="240"
+        height="240"
       />
       <Categories props={categories} />
       <ArticulosRecomendados />

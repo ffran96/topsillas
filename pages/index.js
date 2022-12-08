2c854dc11201f1fa1ec0c85aadb8d69ef885c9a6
Fran <=>
2022-12-08 16:05:56 +0100

    Titulo del inicio y descripcion

--- pages/index.js
+++ pages/index.js
@@ -8,8 +8,8 @@ export default function Home({ categories }) {
     <>
       <SEO
         url="sillas-gamers/mejores-sillas-gaming-por-menos-de-100-euros"
-        title="Inicio"
-        description="Las mejores ofertas, precios y chollos los encontrarás en Top Sillas🔥 TODO lo que quieras saber sobre la silla que buscas lo tienes en Topsillas.info"
+        title="Las mejores reseñas y comparativas"
+        description="Las mejores ofertas, precios y chollos los encontrarás en Top Sillas TODO lo que quieras saber sobre la silla que buscas lo tienes en Topsillas.info"
         img="https://www.topsillas.info/_next/image?url=%2Fsilla-gaming.jpg&w=256&q=75"
         width="644"
         height="1109"

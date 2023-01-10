ce287d0dd00504a38eb94415d8479c0dea956b8d
Fran <=>
2023-01-10 22:19:40 +0100

    Including google tag manager

--- pages/_app.js
+++ pages/_app.js
@@ -2,10 +2,12 @@ import "../styles/globals.css";
 import { ApolloProvider } from "@apollo/client";
 import { client } from "../lib/apollo";
 import { Navbar, Footer } from "/components/Imports";
+import GoogleAnalytics from "@bradgarropy/next-google-analytics";
 
 function MyApp({ Component, pageProps }) {
   return (
     <>
+      <GoogleAnalytics measurementId="G-FJ4E81T7VQ" />
       <ApolloProvider client={client}>
         <Navbar />
         <Component {...pageProps} />
